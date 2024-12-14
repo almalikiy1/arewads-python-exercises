9627b69230294fd36882758210c00663046e4ec8
almalikiy1 <sulaimanilyasu0@gmail.com>
2024-12-14 02:25:13 -0600

    Added these lines from my local machine

---
+++ 02_Module_02_Variables_builtin_functions/variables.py
@@ -0,0 +1,73 @@
+# Day 2: 30 Days of python programming
+
+first_name = 'Sulaiman'
+
+last_name = 'Ilyasu'
+
+full_name = 'Sulaiman Ilyasu'
+
+country = 'Nigeria'
+
+city = 'Kano'
+
+age = 30
+
+year = 2024
+
+is_married = True
+
+is_true = False
+
+is_light_on = True
+
+school, market, hospital = 'BUK', 'RIMI', 'AKTH'
+
+# Level 2 exercises 
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(school))
+print(type(market))
+print(type(hospital))
+
+print(len(first_name))
+print(len(first_name) == len(last_name))
+num_one = 5
+num_two = 4
+total = num_one + num_two
+print(total)
+diff = num_one -num_two
+print(diff)
+product = num_one * num_two
+print(product)
+division = num_one / num_two
+print(division)
+remainder = num_one % num_two
+print(remainder)
+exp = num_one ** num_two
+print(exp)
+floor_division = num_one // num_two
+print(floor_division)
+area_of_circle = 3.142 * 30 * 30
+print(area_of_circle)
+cicum_of_circle = 2 * 3.142 * 30
+print(cicum_of_circle)
+radius = input('Enter the radius: ')
+area_of_circle = 3.142 * int(radius) * int(radius)
+first_name = input('Enter first name: ')
+print(first_name)
+last_name = input('Enter last name: ')
+print(last_name)
+country = input('Enter Country: ')
+print(country)
+age = input('Enter Age: ')
+print(age)
+help(print)
+
